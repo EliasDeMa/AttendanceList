d7abfab7eb5a9e598a0c85a4b896c3e0bbea71f1
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-05-05 11:14:38 +0200

    Show days off in listbox and add ability to delete item

---
+++ AttendanceList/Maintenance/Forms/DaysOffForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Maintenance
+{
+    partial class DaysOffForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.daysOffLabel = new System.Windows.Forms.Label();
+            this.daysOffListBox = new System.Windows.Forms.ListBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addAttenderButton = new System.Windows.Forms.Button();
+            this.nonCourseDayErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nonCourseDayErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.daysOffLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.daysOffListBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.deleteButton);
+            this.splitContainer1.Panel2.Controls.Add(this.addAttenderButton);
+            this.splitContainer1.Size = new System.Drawing.Size(495, 405);
+            this.splitContainer1.SplitterDistance = 279;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // daysOffLabel
+            // 
+            this.daysOffLabel.AutoSize = true;
+            this.daysOffLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.daysOffLabel.Location = new System.Drawing.Point(5, 14);
+            this.daysOffLabel.Name = "daysOffLabel";
+            this.daysOffLabel.Size = new System.Drawing.Size(157, 39);
+            this.daysOffLabel.TabIndex = 1;
+            this.daysOffLabel.Text = "Days Off:";
+            // 
+            // daysOffListBox
+            // 
+            this.daysOffListBox.FormattingEnabled = true;
+            this.daysOffListBox.Location = new System.Drawing.Point(12, 56);
+            this.daysOffListBox.Name = "daysOffListBox";
+            this.daysOffListBox.Size = new System.Drawing.Size(220, 303);
+            this.daysOffListBox.TabIndex = 0;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(22, 125);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(132, 49);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // addAttenderButton
+            // 
+            this.addAttenderButton.Location = new System.Drawing.Point(22, 56);
+            this.addAttenderButton.Name = "addAttenderButton";
+            this.addAttenderButton.Size = new System.Drawing.Size(132, 49);
+            this.addAttenderButton.TabIndex = 2;
+            this.addAttenderButton.Text = "Add";
+            this.addAttenderButton.UseVisualStyleBackColor = true;
+            // 
+            // nonCourseDayErrorProvider
+            // 
+            this.nonCourseDayErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.nonCourseDayErrorProvider.ContainerControl = this;
+            // 
+            // DaysOffForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 429);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "DaysOffForm";
+            this.Text = "DaysOffForm";
+            this.Load += new System.EventHandler(this.DaysOffForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nonCourseDayErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label daysOffLabel;
+        private System.Windows.Forms.ListBox daysOffListBox;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button addAttenderButton;
+        private System.Windows.Forms.ErrorProvider nonCourseDayErrorProvider;
+    }
+}
---
+++ AttendanceList/Maintenance/Forms/DaysOffForm.cs
@@ -0,0 +1,71 @@
+﻿using DatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maintenance
+{
+    public partial class DaysOffForm : Form
+    {
+        private List<NonCourseDay> nonCourseDays;
+        private readonly int _id;
+        public DaysOffForm(int id)
+        {
+            InitializeComponent();
+            _id = id;
+        }
+
+        private void DaysOffForm_Load(object sender, EventArgs e)
+        {
+            LoadData(_id);
+        }
+
+        void LoadData(int id)
+        {
+
+            using (var context = new AttendanceListContext())
+            {
+                nonCourseDays = context.NonCourseDays.Where(x => x.CourseId == id)
+                                       .Select(x => x)
+                                       .ToList();
+            }
+
+            daysOffListBox.Items.Clear();
+            daysOffListBox.Items.AddRange(nonCourseDays.ToArray());
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (daysOffListBox.SelectedIndex == -1)
+            {
+                nonCourseDayErrorProvider.SetError(deleteButton, "Must select item to delete");
+            }
+            else
+            {
+                nonCourseDayErrorProvider.SetError(deleteButton, "");
+                var nonCourseDay = (NonCourseDay)daysOffListBox.SelectedItem;
+
+                var result = MessageBox.Show("Are you sure you want to delete this item", "Delete", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    using (var context = new AttendanceListContext())
+                    {
+                        var toDelete = context.NonCourseDays.FirstOrDefault(a => a.Id == nonCourseDay.Id);
+                        context.NonCourseDays.Remove(toDelete);
+
+                        context.SaveChanges();
+                    }
+                }
+
+                LoadData(_id);
+            }
+        }
+    }
+}
