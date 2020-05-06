097ebcf237386bbc456ce7e06ec0a9ca81be05d2
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-05-06 16:31:26 +0200

    Show messagebox if course has no class at said time.

--- AttendanceList/BadgingSystem/BadgingForm.cs
+++ AttendanceList/BadgingSystem/BadgingForm.cs
@@ -16,6 +16,23 @@ namespace BadgingSystem
         private readonly List<CourseInfo> courses;
         private CourseInfo selectedCourse;
         private List<Attender> attenders;
+        private DateTime beforeAfternoon = new DateTime(
+            DateTime.Now.Year,
+            DateTime.Now.Month,
+            DateTime.Now.Day,
+            13,
+            0,
+            0
+        );
+        private DateTime afterMorning = new DateTime(
+            DateTime.Now.Year,
+            DateTime.Now.Month,
+            DateTime.Now.Day,
+            12,
+            0,
+            0
+        );
+
         public BadgingForm()
         {
             InitializeComponent();
@@ -39,6 +56,32 @@ namespace BadgingSystem
         private void coursesComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             selectedCourse = (CourseInfo)coursesComboBox.SelectedItem;
+            List<NonCourseDay> daysOff;
+            using (var context = new AttendanceListContext())
+            {
+                daysOff = context.NonCourseDays
+                    .Where(x => x.CourseId == selectedCourse.Id).ToList();
+            }
+
+            var dayOff = daysOff.Where(x => x.Date.Value.Date == DateTime.Now.Date).FirstOrDefault();
+
+
+            if (dayOff == default)
+                LoadItems(selectedCourse);
+            else if (dayOff.Morning.Value && dayOff.Afternoon.Value)
+                MessageBox.Show("This course has no class today", "Message", MessageBoxButtons.OK);
+            else if (dayOff.Morning.Value && DateTime.Now < beforeAfternoon)
+                MessageBox.Show("This course has no class in the morning today", "Message", MessageBoxButtons.OK);
+            else if (dayOff.Afternoon.Value && DateTime.Now > afterMorning)
+                MessageBox.Show("This course has no class in the afternoon today", "Message", MessageBoxButtons.OK);
+            else
+                LoadItems(selectedCourse);
+            
+        }
+
+        private void LoadItems(CourseInfo selectedCourse)
+        {
+
             using (var context = new AttendanceListContext())
             {
                 attenders = context.CourseAttenders.Where(c => c.CourseId == selectedCourse.Id)
@@ -64,9 +107,10 @@ namespace BadgingSystem
                 button.Location = new Point(box.Width / 2 - button.Width / 2, box.Height - button.Height - 5);
                 button.Anchor = AnchorStyles.None;
                 button.Click += BadgeInClick;
-                var label = new Label() {
+                var label = new Label()
+                {
                     Text = item.Name,
-                    Anchor = AnchorStyles.None, 
+                    Anchor = AnchorStyles.None,
                     Font = new Font(FontFamily.GenericSansSerif, 9),
                     TextAlign = ContentAlignment.TopCenter,
                     Size = new Size(box.Width - 2, 18),
