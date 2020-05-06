b2bf64d6ab4f3ab7a935a3d31f1af07350bd01a5
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-05-06 10:15:55 +0200

    Add badging system in different project. This allows attenders of a certain course to badge in for the day.

---
+++ AttendanceList/BadgingSystem/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BadgingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.timeShowTimer = new System.Windows.Forms.Timer(this.components);
+            this.coursesComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.timeTextBox.Enabled = false;
+            this.timeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeTextBox.Location = new System.Drawing.Point(562, 13);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(212, 24);
+            this.timeTextBox.TabIndex = 0;
+            this.timeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // timeShowTimer
+            // 
+            this.timeShowTimer.Enabled = true;
+            this.timeShowTimer.Interval = 1000;
+            this.timeShowTimer.Tick += new System.EventHandler(this.timeShowTimer_Tick);
+            // 
+            // coursesComboBox
+            // 
+            this.coursesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.coursesComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coursesComboBox.FormattingEnabled = true;
+            this.coursesComboBox.Location = new System.Drawing.Point(13, 13);
+            this.coursesComboBox.Name = "coursesComboBox";
+            this.coursesComboBox.Size = new System.Drawing.Size(243, 26);
+            this.coursesComboBox.TabIndex = 1;
+            this.coursesComboBox.SelectedIndexChanged += new System.EventHandler(this.coursesComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.coursesComboBox);
+            this.Controls.Add(this.timeTextBox);
+            this.Name = "Form1";
+            this.Text = "Badging System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Timer timeShowTimer;
+        private System.Windows.Forms.ComboBox coursesComboBox;
+    }
+}
+
---
+++ AttendanceList/BadgingSystem/Form1.cs
@@ -0,0 +1,42 @@
+﻿using DatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BadgingSystem
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<CourseInfo> courses;
+        public Form1()
+        {
+            InitializeComponent();
+            using (var context = new AttendanceListContext())
+            {
+                courses = context.CourseInfoes.ToList();
+            }
+        }
+
+        private void timeShowTimer_Tick(object sender, EventArgs e)
+        {
+            timeTextBox.Text = DateTime.Now.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timeTextBox.Text = DateTime.Now.ToString();
+            coursesComboBox.Items.AddRange(courses.ToArray());
+        }
+
+        private void coursesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
